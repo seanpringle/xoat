4a2cb9f9e7cc2b78f91e2ea9877d98bd8bc63ddd
seanpringle <sean.pringle@gmail.com>
2012-09-22 15:50:34 +1000

    config

--- config.h
+++ config.h
@@ -54,8 +54,8 @@
 // Should new windows be automatically focused, or ignored until focused manually?
 // IGNORE means new windows only steal focus if they obscure the current window.
 // STEAL means new windows always steal focus.
-#define FOCUS_START FOCUS_IGNORE
-//#define FOCUS_START FOCUS_STEAL
+//#define FOCUS_START FOCUS_IGNORE
+#define FOCUS_START FOCUS_STEAL
 
 // Available actions...
 // action_move             .num = SPOT1/2/3
@@ -122,6 +122,7 @@ binding keys[] = {
 	// Find or start apps by WM_CLASS (lower case match).
 	// Only works for apps that use some form of their binary name as their class...
 	{ .mod = AnyModifier, .key = XK_F1, .act = action_find_or_start, .data = "konsole"  },
-	{ .mod = AnyModifier, .key = XK_F2, .act = action_find_or_start, .data = "chromium" },
+	{ .mod = AnyModifier, .key = XK_F2, .act = action_find_or_start, .data = "uzbl-tabbed" },
 	{ .mod = AnyModifier, .key = XK_F3, .act = action_find_or_start, .data = "pcmanfm"  },
+	{ .mod = AnyModifier, .key = XK_F4, .act = action_find_or_start, .data = "kate" },
 };
